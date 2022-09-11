b4b4152a9b257d2c5f52e4c82ec1e85e0a34606e
Yo o <joel.huillca@alumnos.ucn.cl>
2022-09-11 20:33:21 -0300

    Arreglos

--- app/src/main/java/joel/vcodelabandroid/ejercicioclase/SecondFragment.java
+++ app/src/main/java/joel/vcodelabandroid/ejercicioclase/SecondFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -15,13 +18,27 @@ public class SecondFragment extends Fragment {
 
     private FragmentSecondBinding binding;
 
+    private EditText txt_number1, txt_number2;
+
+    private RadioButton radio_suma,  radio_resta, radio_multiplicaion, radio_dividir;
+
+    private RadioGroup radio_group;
+
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        /****** Instaciar componentes ******/
 
+        //Binding: Es la variable que nos permite acceder a nuestra puerta visual
         binding = FragmentSecondBinding.inflate(inflater, container, false);
+        txt_number1=(EditText) binding.txtNumber1;
+        txt_number2=(EditText) binding.txtNumber2;
+
+
+
         return binding.getRoot();
 
     }
